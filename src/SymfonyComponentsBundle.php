bc5eac93810e313fd8d1f206e93f2c14e86cb894
Martin <hello@northrook.com>
2024-03-11 19:50:07 +0000

    mend

---
+++ src/SymfonyComponentsBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Northrook\Components\Latte;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @version 1.0 ☑️
+ * @author Martin Nielsen <mn@northrook.com>
+ *
+ * @link https://github.com/northrook Documentation
+ * @todo Update URL to documentation : root of symfony-latte-bundle
+ */
+final class SymfonyLatteBundle extends Bundle
+{
+	public function getPath() : string {
+		return dirname( __DIR__ );
+	}
+}
