fad716c07374af8c7479ce5cd5bfeb7f4ad60129
Martin <hello@northrook.com>
2024-05-12 17:13:25 +0100

    mend

--- config/services.php
+++ config/services.php
@@ -5,11 +5,14 @@ declare( strict_types = 1 );
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Northrook\Symfony\Components\LatteComponentPreprocessor;
+use Northrook\Symfony\Core\File;
 
 return static function ( ContainerConfigurator $container ) {
 
     $services   = $container->services();
     $parameters = $container->parameters();
+
+    $parameters->set( 'dir.components.templates', File::parameterDirname( '../../templates' ) );
 
     /** # ☕
      * Latte Preprocessor
